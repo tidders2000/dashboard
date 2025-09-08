a43c944a23b2f86712dc88497ac7465e51c0a6fc
tidders2000 <simon.tid@btinternet.com>
2025-09-08 15:57:22 +0100

    changed db settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 
+
+
 from pathlib import Path
 
 from decouple import config
@@ -31,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['dashboard-xyco.onrender.com']
+ALLOWED_HOSTS = ['dashboard-xyco.onrender.com','127.0.0.1']
 
 
 # Application definition
@@ -81,21 +83,25 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     "default": dj_database_url.config(
-#         default="sqlite:///db.sqlite3",
+#         default=config("DATABASE_URL"),
 #         conn_max_age=600,
 #         ssl_require=True
 #     )
 # }
 
+
+DATABASES = {
+    'default': dj_database_url.parse(config("DATABASE_URL"))
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
